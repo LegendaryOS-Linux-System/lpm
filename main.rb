c686bc61b8f0d1258cacc4e478bfa1ebc9678c77
michal92299 <chudybyczek2137@gmail.com>
2026-05-12 23:03:26 +0200

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,1 +1,13 @@
+require_relative 'src/ui'
+require_relative 'src/bootc'
+require_relative 'src/commands'
+require_relative 'src/spinner'
 
+module LPM
+  VERSION = '1.0.0'
+  NAME    = 'lpm'
+  DISTRO  = 'LegendaryOS'
+end
+
+UI.init
+Commands.run(ARGV)
